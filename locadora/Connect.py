e8f2be442c0860ac484d4bf057d9e89171caa3fa
Breno Souza Costa <32497952+brenoSCosta@users.noreply.github.com>
2019-09-13 23:04:56 -0300

    Initial commit

---
+++ locadora/Connect.py
@@ -0,0 +1,22 @@
+import cx_Oracle
+from locadora import EtlLocadora as tl
+ip = '127.0.0.1'
+port = 1521
+SID = 'xe'
+dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+
+
+con = cx_Oracle.connect('LOCADORA', '123', dsn_tns)
+cur = con.cursor()
+conDW = cx_Oracle.connect('DW_LOCADORA','123', dsn_tns)
+curDW = conDW.cursor()
+
+#Teste pro git
+tl.gravadora(cur, curDW)
+tl.socio(cur, curDW)
+
+conDW.commit()
+curDW.close()
+conDW.close()
+cur.close()
+con.close()
---
+++ locadora/EtlLocadora.py
@@ -0,0 +1,45 @@
+
+import cx_Oracle
+
+def gravadora(cur, curDW):
+    checker = None
+    cur.execute('select * from GRAVADORAS')
+    for result in cur:
+        statement = 'insert into DM_GRAVADORA(ID_GRAV,UF_GRAV,NAC_BRAS,NOM_GRAV) ' \
+                    'VALUES ( :0, :1, :2, :3)'
+        try:
+            curDW.execute(statement, (result[0], result[1], result[2], result[3]))
+        except cx_Oracle.IntegrityError:
+            print("Erro!! Cadastros já existentes na DM_GRAVADORA")
+            checker=True
+            break
+    if checker == None :
+        print("Dados ETL realizados com sucesso. DM_GRAVADORA")
+
+
+
+def socio(cur,curDW):
+    checker = None
+    cur.execute('SELECT * FROM SOCIOS')
+    di={}
+    i=0
+    for result in cur:
+        dsc=''
+        if result[2]==1:
+            dsc='Comum'
+        elif result[2]==2:
+            dsc='Especial'
+        elif result[2]==3:
+            dsc='Vip'
+        di[i]=[result[0],result[4],dsc]
+        i+=1
+    for ii in di:
+        statement = 'insert into DM_SOCIO(ID_SOC,NOM_SOC,TIPO_SOCIO) VALUES (:0,:1,:2)'
+        try:
+            curDW.execute(statement, (di[ii][0],di[ii][1],di[ii][2]))
+        except cx_Oracle.IntegrityError:
+            print("Erro!! Cadastros já existente na DM_SOCIO")
+            checker = True
+            break
+    if checker == None:
+        print("Dados ETL realizados com sucesso. DM_SOCIO")
